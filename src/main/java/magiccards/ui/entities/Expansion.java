6bf698b1dfe1b944868788aa98c74fa16ad71b7e
Gustavo Gomes <gustavo.sgomess@gmail.com>
2019-07-27 15:35:13 -0300

    Criando pagina extension

---
+++ src/main/java/magiccards/ui/entities/Expansion.java
@@ -0,0 +1,21 @@
+package magiccards.ui.entities;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Expansion {
+	private Integer expansionId;
+	private String name;
+	private String ptBrName;
+	private String linkName;
+	private String searchName;
+	private String code;
+	private LocalDate launchDate;
+	private Integer expansionCategoryId;
+	private Boolean isPromo;
+	private Boolean isLegal;
+}
