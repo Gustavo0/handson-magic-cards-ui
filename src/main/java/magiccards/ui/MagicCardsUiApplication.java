9e32c2d7c87710ac13e78b78e4a0e7f23c849730
Gustavo Gomes <gustavo.sgomess@gmail.com>
2019-07-28 02:07:41 -0300

    AJustes

--- src/main/java/magiccards/ui/MagicCardsUiApplication.java
+++ src/main/java/magiccards/ui/MagicCardsUiApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+import com.netflix.hystrix.HystrixCommandProperties;
+
 @SpringBootApplication
 @EnableFeignClients
 public class MagicCardsUiApplication {
     public static void main(String[] args) {
+    	HystrixCommandProperties.Setter().withExecutionTimeoutEnabled(false);
         SpringApplication.run(MagicCardsUiApplication.class, args);
     }
 }
